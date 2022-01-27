221793133b4d9af355ce64d8184be9dece12169f
Amagash <tiffanysouterre@gmail.com>
2022-01-27 16:49:09 +0100

    lighten commands

--- context/1.0-commands.js
+++ context/1.0-commands.js
@@ -34,32 +34,11 @@ bot.setControlState('sprint', true);
 bot.setControlState('jump', true);
 bot.setControlState('jump', false);
 
-// Look at me
-watchTarget();
-
-// Look at me continously
-watchInterval = setInterval(watchTarget, 50)
-
-// Stop looking at me
-clearInterval(watchInterval)
-
-// Ok, keep looking at me again
-watchInterval = setInterval(watchTarget, 50)
-
-// Ahh stop it, that's creepy
-clearInterval(watchInterval)
-
 // Jump several times
 bot.setControlState('jump', true)
 setTimeout(() => {
   bot.setControlState('jump', false)
 }, 3000);
-
-// Okay, look at me
-watchInterval = setInterval(watchTarget, 50)
-
-// Enough! That's terrible!
-clearInterval(watchInterval)
 
 // Let's go to the right
 bot.setControlState('right', true)
