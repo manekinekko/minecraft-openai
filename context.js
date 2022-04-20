2ac45eb196311239b9535810d398272acdf23571
Amagash <tiffanysouterre@gmail.com>
2022-04-20 03:36:04 +0200

    chore: enriching context

--- context.js
+++ context.js
@@ -1,26 +1,60 @@
 const defaultContext = [
-  "// Go forward",
-  "bot.setControlState('forward', true);",
+  `// Go forward
+  bot.setControlState('forward', true);
   
-  "// Go back",
-  "bot.setControlState('back', true);",
+  // Go back
+  bot.setControlState('back', true);
+
+  // jump
+  bot.setControlState('jump', true);
   
-  "// Hi how are you ?",
-  `bot.chat("I'm fine, thanks!");`,
+  // Hi how are you ?
+  bot.chat("I'm fine, thanks!");
   
-  "// What's your name ?",
-  `bot.chat("My name is " + bot.username);`,
+  // What's your name ?
+  bot.chat("My name is " + bot.username);
+
+  // stop
+  bot.clearControlStates();
+
+  // stop it
+  bot.clearControlStates();
+
+  // come to me
+  findPlayer(bot, 2, target);
+
+  // follow me 
+  goToPlayerInterval = setInterval(() => followPlayer(bot, 2, target), 1000);
+
+  // stop following me
+
+  clearInterval(goToPlayerInterval);
+
+  // look at me
+  watchPlayer(target, bot);
+
+  // keep looking at me
+  watchInterval = setInterval(() => watchPlayer(target, bot), 1000);
+
+  // stop looking at me
+  clearInterval(watchInterval[0])
+
+  // what is my name?
+  bot.chat("Your name is " + username);
   
-  "// What's your age ?",
-  `bot.chat("I'm " + bot.entity.age + " years old");`,
-  
-  // crafting
-  "// Craft sticks",
-  `const plankRecipe = bot.recipesFor(mcData.itemsByName.oak_planks.id ?? mcData.itemsByName.planks.id)[0];`,
-  `await bot.craft(plankRecipe, 1, null);`,
-  `const stickRecipe = bot.recipesFor(mcData.itemsByName.sticks.id)[0];`,
-  `await bot.craft(stickRecipe, 1, null);`,
-  `bot.chat('Crafting Sticks finished');`,
+  // Get dirt
+  mineBlock (bot, "dirt", mcData);
+
+  // Get oak log
+  mineBlock (bot, "oak_log");
+
+  // Craft sticks,
+  const plankRecipe = bot.recipesFor(mcData.itemsByName.oak_planks.id ?? mcData.itemsByName.planks.id)[0];
+  await bot.craft(plankRecipe, 1, null);
+  const stickRecipe = bot.recipesFor(mcData.itemsByName.sticks.id)[0];
+  await bot.craft(stickRecipe, 1, null);
+  bot.chat('Crafting Sticks finished');
+  `
 ];
 
 /**
